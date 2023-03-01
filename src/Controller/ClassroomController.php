277bd4e76082629f042c01bfb8f40adedcfaba86
Naderab <abdellaouinader@gmail.com>
2023-03-01 17:43:06 +0100

    Seance 5 crud + jointure

--- src/Controller/ClassroomController.php
+++ src/Controller/ClassroomController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Classroom;
+use App\Repository\ClassroomRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,10 +16,51 @@ class ClassroomController extends AbstractController
     {
         $repo = $doctrine->getRepository(Classroom::class);
         $classrooms=$repo->findAll();
-        var_dump($classrooms);
-        die();
         return $this->render('classroom/index.html.twig', [
             'controller_name' => 'ClassroomController',
+            'classrooms'=>$classrooms
         ]);
+    }
+    // #[Route('/deleteClassroom/{id}',name: 'classroom_delete')]
+    // public function deleteClassroom($id,ClassroomRepository $repo,ManagerRegistry $doctrine){
+    //     $classroom = $repo->find($id);
+    //     $entitymanager = $doctrine->getManager();
+    //     $entitymanager->remove($classroom);
+    //     $entitymanager->flush();
+    //     return $this->redirectToRoute('app_classroom');
+    // }
+
+    #[Route('/deleteClassroom/{id}',name: 'classroom_delete')]
+    public function deleteClassroom($id,ClassroomRepository $repo){
+        $classroom = $repo->find($id);
+        $repo->remove($classroom,true);
+        return $this->redirectToRoute('app_classroom');
+    }
+    // #[Route('/addClassroom',name: 'classroom_add')]
+    // public function addClassroom(ClassroomRepository $repo){
+    //     $classroom = new Classroom();
+    //     $classroom->setUsername('3A21');
+    //     $classroom->setEmail('3A21@esprit.tn');
+    //     $repo->save($classroom,true);
+    //     return $this->redirectToRoute('app_classroom');
+    // }
+    #[Route('/addClassroom',name: 'classroom_add')]
+    public function addClassroom(ClassroomRepository $repo,ManagerRegistry $doctrine){
+        $classroom = new Classroom();
+        $classroom->setUsername('3A21');
+        $classroom->setEmail('3A21@esprit.tn');
+        $entitymanager=$doctrine->getManager();
+        $entitymanager->persist($classroom);
+        $entitymanager->flush();
+        return $this->redirectToRoute('app_classroom');
+    }
+
+    #[Route('/updateClassroom/{id}',name: 'classroom_update')]
+    public function updateClassroom($id,ClassroomRepository $repo,ManagerRegistry $doctrine){
+        $classroom = $repo->find($id);
+        $classroom->setUsername('classroom updated');
+        $entitymanager = $doctrine->getManager();
+        $entitymanager->flush();
+        return $this->redirectToRoute('app_classroom');
     }
 }
